ccb9626dbb9403d69e067908c0c49f01663779b1
Omar Luna Hernández <42101656@uaz.edu.mx>
2025-11-11 20:09:55 -0600

    feat: Add foreign key relationship to Solicitud in SeguimientoSolicitud model

--- app/solicitudes/tipo_solicitudes/models.py
+++ app/solicitudes/tipo_solicitudes/models.py
@@ -85,6 +85,7 @@ ESTATUS = [
     ('4', 'Cancelada'),
 ]
 class SeguimientoSolicitud(models.Model):
+    solicitud = models.ForeignKey(Solicitud, on_delete=models.CASCADE, related_name='seguimientos')
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     observaciones = models.TextField(blank=True)
     estatus = models.CharField(max_length=1, choices=ESTATUS)
