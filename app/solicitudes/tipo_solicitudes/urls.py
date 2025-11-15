efc78f4e4bf3d80bb4e99631012c90d7112b89d8
Luis Javier Vargas Ortiz <luisitog453@gmail.com>
2025-11-14 21:18:03 -0600

    agregando boton para tipo de solicitud

--- app/solicitudes/tipo_solicitudes/urls.py
+++ app/solicitudes/tipo_solicitudes/urls.py
@@ -3,7 +3,7 @@ from tipo_solicitudes import views
 
 'tipo-solicitud/'
 urlpatterns = [
-    path('', views.agregar, name='agrega_solicitud'),
-    path('lista', views.lista_solicitudes, name='lista_tipo_solicitudes'),
-    path('crear-formulario', views.crear_formulario, name='crear_formulario'),
+    path('', views.lista_solicitudes, name='lista_tipo_solicitudes'),  
+    path('agregar/', views.agregar, name='agrega_solicitud'),
+    path('crear-formulario/', views.crear_formulario, name='crear_formulario'),
 ]
